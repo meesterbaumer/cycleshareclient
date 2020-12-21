d4ddc51a37f4c4bfef08ee159c68c530de91d60f
Michael Baumer <meeesterbaumer@gmail.com>
2020-12-21 11:15:05 -0600

    Added bike provider with full CRUD

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,6 +4,7 @@ import "./NavBar.css"
 
 export const NavBar = (props) => {
     return (
+        
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/">Dashboard</Link>
