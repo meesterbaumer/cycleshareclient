2580ee1b89326832f2829c799d975addc734a517
Michael Baumer <meeesterbaumer@gmail.com>
2020-12-28 15:23:35 -0600

    modal pops up and some content is rendered

--- src/components/dashboard/DashboardList.js
+++ src/components/dashboard/DashboardList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import { RiderContext } from "../rider/RiderProvider"
 import { SearchForm } from "../dashboard/SearchForm"
 import "./Dashboard.css"
@@ -6,16 +6,65 @@ import { MyBikesList } from "../bike/MyBikes"
 
 export const DashboardList = () => {
 
+    const addBikeDialog = useRef()
+    const year = useRef()
+    const make = useRef()
+    const model = useRef()
+
+    const addBikeClicked = () => addBikeDialog.current.showModal()
+
     return (
         <>
+            {/* Initial render of dashboard content */}
             <h3 className="greeting">Hello *username*, Welcome to CyCleShare</h3>
             <SearchForm />
             <div className='upcomingRides'>Upcoming Rides</div>
             <div className='myGarage'>Garage
-            <button className='addBikeButton'>Add a Bike</button>
+            <button onClick={addBikeClicked} className='addBikeButton'>Add a Bike</button>
                 <MyBikesList />
             </div>
             <div className='myReviews'>Reviews</div>
+
+            {/* Dialog for adding a bike to your garage */}
+            <dialog className="addBikeDialog" ref={addBikeDialog}>
+                <div>Enter Your Bike's Details</div>
+                <form>
+                    <fieldset>
+                        <label>Year</label>
+                        <input
+                            ref={year}
+                            type="number"
+                            name="year"
+                            className="form-control"
+                            placeholder="2020"
+                            required
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <label>Make</label>
+                        <input
+                            ref={make}
+                            type="text"
+                            name="make"
+                            className="form-control"
+                            placeholder="Trek"
+                            required
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <label>Model</label>
+                        <input
+                            ref={model}
+                            type="text"
+                            name="model"
+                            className="form-control"
+                            placeholder="Madone"
+                            required
+                        />
+                    </fieldset>
+                    
+                </form>
+            </dialog>
         </>
     )
 }
