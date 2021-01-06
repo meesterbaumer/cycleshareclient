1ac5b87c37450d6930e42af658d85e0f890e17d0
Michael Baumer <meeesterbaumer@gmail.com>
2021-01-06 09:59:09 -0600

    Reviews now work

--- src/components/reservation/ReservationReview.js
+++ src/components/reservation/ReservationReview.js
@@ -1,7 +1,27 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { ReviewContext } from "../reviews/ReviewProvider"
 import "./Reservation.css"
 
 export const ReservationReview = ({ reservation }) => { 
+
+    const { addReviews } = useContext(ReviewContext)
+
+    const [review, setReview] = useState({
+        message: ""
+    })
+
+    const handleControlledInputChange = (event) => {
+        const newReview = Object.assign({}, review)
+        newReview[event.target.name] = event.target.value
+        setReview(newReview)
+    }
+
+    const addReview = () => {
+        addReviews({
+            message : review.message,
+            user : reservation.bike.rider.id
+        })
+    }
 
     return (
         <>
@@ -9,11 +29,16 @@ export const ReservationReview = ({ reservation }) => {
                 <div className="reviewFirstName">{reservation.bike.rider.user.first_name}</div>
                 <div className="reviewFirstName">{reservation.bike.rider.user.last_name}</div>
                 <div className="reviewMessage">Thank {reservation.bike.rider.user.first_name} for the ride</div>
-                <input
-                type="text"
-
-                >
-                </input>
+                <textarea
+                name="message"
+                id="message"
+                onChange={handleControlledInputChange}
+                rows="5"
+                cols="15"
+                placeholder="Type your message here"
+                required
+                />
+                <button onClick={addReview}>Submit Review</button>
             </div>
         </>
     )
